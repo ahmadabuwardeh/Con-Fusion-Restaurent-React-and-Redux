c7fd9f1423c4993119dcac247c1c3461ab351d00
ahmadabuwardeh <ahmad_azmi95@hotmail.com>
2019-12-19 02:42:08 +0200

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import DishDetail from "./DishdetailComponent";
 import Home from './HomeComponent';
+import About from'./AboutComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponents';
 import Footer from './FooterComponents';
@@ -52,6 +53,7 @@ class Main extends Component{
                         <Route path='/home' component={HomePage}/>
                         <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/>}/>
                         <Route path='/menu/:dishId' component={DishWithId}/>
+                        <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders} />}/>
                         <Route exact path='/contactus' component={Contact} />
                         <Redirect to='/home'/>
                     </Switch>
